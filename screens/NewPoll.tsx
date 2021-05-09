985f77751286aba363d75753fabd844a579f942d
Artem Golendukhin <artem.golendukhin@gmail.com>
2021-05-09 14:57:06 +0300

    added gradient

--- screens/NewPoll.tsx
+++ screens/NewPoll.tsx
@@ -4,12 +4,37 @@
 */
 
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import {black, blue} from '../colors';
+
+const Header = () => <View style={{height: 64, backgroundColor: black}}></View>;
 
 export default () => {
   return (
-    <View>
-      <Text>New poll</Text>
+    <View style={{flex: 1}}>
+      <Header></Header>
+      <View style={styles.gradientContainer}>
+        <LinearGradient
+          useAngle={true}
+          angle={154}
+          angleCenter={{x: 0, y: 0}}
+          colors={['#2467F4', '#14131B']}
+          style={styles.gradient}></LinearGradient>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  gradientContainer: {
+    flex: 1,
+    backgroundColor: black,
+  },
+  gradient: {
+    flex: 1,
+    opacity: 0.12,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+});
